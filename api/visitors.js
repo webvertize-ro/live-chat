b694feb8e697d96ff5c5c99722fa3cd3e3e4c290
webvertize-ro <contact@webvertize.ro>
2026-01-22 08:44:53 +0200

    further changes

--- api/visitors.js
+++ api/visitors.js
@@ -32,30 +32,31 @@ export default async function handler(req, res) {
     }
 
     // Inserting the pre-defined message in the messages table
-    // const { dataM, errorM } = await supabase
-    //   .from('messages')
-    //   .insert([
-    //     {
-    //       user_name: 'Edion Trans',
-    //       message: 'Buna ziua! Cu ce va putem ajuta astazi?',
-    //       sender_type: 'admin',
-    //       visitor_id: visitor_id,
-    //       type: 'text',
-    //     },
-    //   ])
-    //   .select()
-    //   .single();
+    const { dataM, errorM } = await supabase
+      .from('messages')
+      .insert([
+        {
+          user_name: 'Edion Trans',
+          message: 'Buna ziua! Cu ce va putem ajuta astazi?',
+          sender_type: 'admin',
+          visitor_id: data.id,
+          type: 'text',
+        },
+      ])
+      .select()
+      .single();
 
-    // if (errorM) {
-    //   console.error(error);
-    //   return res.status(500).json({
-    //     error: 'Error inserting the pre-defined message in the database.',
-    //   });
-    // }
+    if (errorM) {
+      console.error(error);
+      return res.status(500).json({
+        error: 'Error inserting the pre-defined message in the database.',
+      });
+    }
 
     return res.status(200).json({
       success: true,
       visitor: data,
+      initialMessage: dataM,
     });
   }
 
